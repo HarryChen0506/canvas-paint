d3f939173e38a05bc5f030e0a2d572050e956195
chenwenhua <wenhua.chen@versa-ai.com>
2018-07-08 11:41:19 +0800

    运动的小球

--- utils/tool.js
+++ utils/tool.js
@@ -46,6 +46,8 @@ function Ball(radius, color) {
   this.radius = radius || 30
   this.x = 0
   this.y = 0
+  this.vx = 0
+  this.vy = 0
   this.scaleX = 1
   this.scaleY = 1
   this.color = color || '#f00'
@@ -56,6 +58,11 @@ Ball.prototype = {
     this.y = y
     return this
   },
+  move: function (vx, vy) {
+    this.vx = vx
+    this.vy = vy
+    return this
+  },
   scale: function (scaleX, scaleY) {
     this.scaleX = scaleX
     this.scaleY = scaleY
@@ -64,13 +71,13 @@ Ball.prototype = {
   draw: function (context) {
     context.save()
     context.translate(this.x, this.y)
-    context.lineWidth = 5
+    context.lineWidth = 0
     context.fillStyle = this.color
     context.scale(this.scaleX, this.scaleY)
     context.beginPath()
     context.arc(0, 0, this.radius, 0, Math.PI*2, false)
     context.closePath()
-    context.stroke() //描边 
+    // context.stroke() //描边 
     context.fill() //填充 
     context.restore()
     return this
